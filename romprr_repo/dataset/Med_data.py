6a27d82c648a138a1882fb88e75f499de37b8a56
Romain <romprr03@gmail.com>
2026-03-10 14:19:42 +0100

    Add: experiment for lab 3 with dataset loader, currently needs ucimlrepo package but since well move to the kaggle version i'll have to update the code

---
+++ romprr_repo/dataset/Med_data.py
@@ -0,0 +1,63 @@
+import os
+from sklearn.discriminant_analysis import StandardScaler
+import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from fluke.data.datasets import DataContainer
+from ucimlrepo import fetch_ucirepo 
+
+# Need to update to just clean up the dataset (pull from kaggle & remove rows with label 1 and change label 2 to one after)
+ 
+def CDC_DIABETES_INDICATORS(**kwargs) -> DataContainer :
+
+    data_path = kwargs.get('path')
+
+    os.makedirs(data_path, exist_ok=True)
+
+    # Define file paths for features and targets
+    x_file = os.path.join(data_path, 'X.csv')
+    y_file = os.path.join(data_path, 'y.csv')
+
+    if os.path.exists(x_file) and os.path.exists(y_file):
+        print(f"Loading dataset from local cache: {data_path}")
+        X = pd.read_csv(x_file)
+        y = pd.read_csv(y_file)
+
+        print(f"Data set of {len(X)} columns.")
+
+    else:
+        print('Downloading CDC DIABTEHES HEALTH INDICATORS from UCIML repository')
+        cdc_diabetes_health_indicators = fetch_ucirepo(id=891) 
+    
+        # data (as pandas dataframes) 
+        X = cdc_diabetes_health_indicators.data.features 
+        y = cdc_diabetes_health_indicators.data.targets 
+
+        print(f"Data set of {len(X)} columns.")
+
+        X.to_csv(x_file, index=False)
+        y.to_csv(y_file, index=False)
+        print(f"Dataset saved to: {data_path}")
+
+    # There are no categorical data and no missing data
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    scaler = StandardScaler()
+    # Fit on training data, transform both
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    # There are 3 classes : (0) Healthy, (1) Prediabete, (2) Diabete
+    return DataContainer(
+        X_train=torch.tensor(X_train.to_numpy(), dtype=torch.float32),
+        y_train=torch.tensor(y_train.to_numpy(), dtype=torch.long).squeeze(),
+        X_test=torch.tensor(X_test.to_numpy(), dtype=torch.float32),
+        y_test=torch.tensor(y_test.to_numpy(), dtype=torch.long).squeeze(),
+        num_classes=3
+    )
+
+if __name__ == '__main__' :
+    CDC_DIABETES_INDICATORS(path = "/home/romain/dev/Master/federated_learning/fl-lab/data")
